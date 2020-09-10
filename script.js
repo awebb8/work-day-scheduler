612c96ecc99299e394b48020eb654de19a00212e
Alkawe <alixkwebb@gmail.com>
2020-09-10 19:42:12 -0400

    added a for loop in order to create a new row for each hour from 9am to 5pm

--- script.js
+++ script.js
@@ -7,9 +7,38 @@ $(document).ready(function() {
     
 });
 
-// TODO: WHEN I scroll down, I am presented with timeblocks for standard business hours
+//  Timeblocks for standard business hours
+// for loop where i is the hour.  This loop creates a new row and adds the time to each one.
+for (var i = 9; i <=17; i++) {
+    let hourIndex = i;
+    console.log(hourIndex);
+
+    var hour = 0;
+
+    if (hourIndex < 12) {
+        hour = hourIndex + "am"
+        console.log(hour);
+    }
+    else if (hourIndex == 12) {
+        hour = hourIndex + "pm"
+        console.log(hour);
+    }
+    else if (hourIndex > 12) {
+        hour = (hourIndex - 12) + "pm";
+        console.log(hour);
+    }
+
+    let timeDisplay = $("#time-display");
+    timeDisplay.text(hour);
+
+    $("#time-text-save").clone().removeClass("hide").appendTo("#repeat-rows");
+
+    console.log("repeat row");
+}
 
 // TODO: WHEN I view the timeblocks for that day, each timeblock is color coded to indicate whether it is in the past, present, or future
+// let currentHour = moment().format('H');
+// console.log(currentHour);
 
 // TODO: WHEN I click into a timeblock, I can enter an event
 
